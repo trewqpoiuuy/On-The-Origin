c6360d76a6bb2859a991ebd9a90b406495ff5921
trewqpoiuuy <bknutson@bayschoolsf.org>
2014-01-18 19:41:56 -0800

    added tree struct, moved things into seperate functions

--- Tree generation test.cpp
+++ Tree generation test.cpp
@@ -4,10 +4,8 @@
 #include <ctime>
 #include <Tree generation.h>
 using namespace std;
-int main()
+seed generateSeed()
 {
-	srand(time(NULL));
-	int uselessThing=rand(); //First value of rand is the seed, so this gets rid of it
 	seed treeSeed;
 	/*
 		~See Tree generation.h for the significance of these values~
@@ -30,21 +28,64 @@ int main()
 	cout << "Secondary: " << treeSeed.secondaryColor[0] << " " << treeSeed.secondaryColor[1] << " " << treeSeed.secondaryColor[2] << endl;
 	cout << "Tertiary: " << treeSeed.tertiaryColor[0] << " " << treeSeed.tertiaryColor[1] << " " << treeSeed.tertiaryColor[2] << endl;
 	cout << "Branch Density: " << treeSeed.branchDensity << " Angle Variance: " << treeSeed.angleVariance << " Feature Chance: "<< treeSeed.featureChance << " Length Variance: " << treeSeed.lengthVariance << endl;
-	int sunlight=0;   //
-	int water=0;      //
-	int phosphorus=0; //Resources
-	int nitrogen=0;   //
-	int potassium=0;  //
-	bool isAlive=1;
-	vector<branch> tree; //Stores all the branches
+	return treeSeed;
+}
+tree spawnTree(int x, int y,int z,seed treeSeed)
+{	
+	tree newTree;
+	newTree.sunlight=0;   //
+	newTree.water=0;      //
+	newTree.phosphorus=0; //Resources
+	newTree.nitrogen=0;   //
+	newTree.potassium=0;  //
+	newTree.isAlive=1;
+	newTree.x=x;
+	newTree.y=y;
+	newTree.z=z;
+	newTree.treeSeed=treeSeed;
 	branch trunk;
 	trunk.connection=0;
 	trunk.xAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
 	trunk.yAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
 	trunk.length=randFloat(1,40);
 	trunk.feature=0;
-	tree.push_back(trunk);
-	while(isAlive==1)
+	newTree.branches.push_back(trunk);
+	return newTree;
+}
+tree growBranch(tree newTree)
+{
+	branch newBranch;
+	int branchWeighting=randInt(0,newTree.branches.size())*newTree.treeSeed.branchDensity; //Weights connection points
+	if(branchWeighting>=newTree.branches.size())
+	{
+		branchWeighting=newTree.branches.size()-1; //Makes sure branches only connect to existing branches
+	}
+	newBranch.connection=(newTree.branches.size()-branchWeighting);
+	newTree.branches.at(newBranch.connection-1).children.push_back(newTree.branches.size()+1);
+	newBranch.xAngle=randFloat(-newTree.treeSeed.angleVariance,newTree.treeSeed.angleVariance);
+	newBranch.yAngle=randFloat(-newTree.treeSeed.angleVariance,newTree.treeSeed.angleVariance);
+	newBranch.length=randFloat(1,20)*newTree.treeSeed.lengthVariance;
+	newBranch.feature=0;
+	int featureChance=rand()%100;
+	if(featureChance/100<newTree.treeSeed.featureChance)
+	{
+		newBranch.feature=randInt(1,3);
+	}
+	newTree.branches.push_back(newBranch);
+	newTree.sunlight=newTree.sunlight-newBranch.length/2;
+	newTree.water=newTree.water-newBranch.length;
+	newTree.nitrogen=newTree.nitrogen-newBranch.length/1.5; //Resource usage based solely on length for now. This will likely change.
+	newTree.potassium=newTree.potassium-newBranch.length*1.5;
+	newTree.phosphorus=newTree.phosphorus-newBranch.length*1.25;
+	return newTree;
+}
+int main()
+{
+	srand(time(NULL));
+	int uselessThing=rand(); //First value of rand is the seed, so this gets rid of it
+	seed treeSeed=generateSeed();
+	tree newTree=spawnTree(0,0,0,treeSeed);
+	while(newTree.isAlive==1)
 	{
 		int sunlightAdded;
 		int waterAdded;
@@ -53,60 +94,38 @@ int main()
 		int nitrogenAdded;
 		cout << "sunlight this turn?" << endl;
 		cin >> sunlightAdded;
-		sunlight=sunlight+sunlightAdded;
+		newTree.sunlight=newTree.sunlight+sunlightAdded;
 		cout << "water this turn?" << endl;
 		cin >> waterAdded;
-		water=water+waterAdded;
+		newTree.water=newTree.water+waterAdded;
 		cout << "potassium this turn?" << endl;				//Prompts are temporary, will be replaced by game engine commands
 		cin >> potassiumAdded;
-		potassium=potassium+potassiumAdded;
+		newTree.potassium=newTree.potassium+potassiumAdded;
 		cout << "phosphorus this turn?" << endl;
 		cin >> phosphorusAdded;
-		phosphorus=phosphorus+phosphorusAdded;
+		newTree.phosphorus=newTree.phosphorus+phosphorusAdded;
 		cout << "nitrogen this turn?" << endl;
 		cin >> nitrogenAdded;
-		nitrogen=nitrogen+nitrogenAdded;
-		while(sunlight>=10 && water>=20 && nitrogen>=15 && potassium>=30 && phosphorus>=25)
-		{
-			branch newBranch;
-			int branchWeighting=randInt(0,tree.size())*treeSeed.branchDensity; //Weights connection points
-			if(branchWeighting>=tree.size())
+		newTree.nitrogen=newTree.nitrogen+nitrogenAdded;
+			while(newTree.sunlight>=10 && newTree.water>=20 && newTree.nitrogen>=15 && newTree.potassium>=30 && newTree.phosphorus>=25)
 			{
-				branchWeighting=tree.size()-1; //Makes sure branches only connect to existing branches
+				newTree=growBranch(newTree);
 			}
-			newBranch.connection=(tree.size()-branchWeighting);
-			tree.at(newBranch.connection-1).children.push_back(tree.size()+1);
-			newBranch.xAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
-			newBranch.yAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
-			newBranch.length=randFloat(1,20)*treeSeed.lengthVariance;
-			newBranch.feature=0;
-			int featureChance=rand()%100;
-			if(featureChance/100<treeSeed.featureChance)
-			{
-				newBranch.feature=randInt(1,3);
-			}
-			tree.push_back(newBranch);
-			sunlight=sunlight-newBranch.length/2;
-			water=water-newBranch.length;
-			nitrogen=nitrogen-newBranch.length/1.5; //Resource usage based solely on length for now. This will likely change.
-			potassium=potassium-newBranch.length*1.5;
-			phosphorus=phosphorus-newBranch.length*1.25;
-		}
-		for(int f=0;f<tree.size();f++) //prints out tree data
+		for(int f=0;f<newTree.branches.size();f++) //prints out tree data
 		{
 			cout << "Branch #"  << f+1 << ": " ;
-			cout << "Connection Point: " << tree.at(f).connection << " ";
-			cout << "X Angle: " << tree.at(f).xAngle << " ";
-			cout << "Y Angle: " << tree.at(f).yAngle << " ";
-			cout <<  "Length: " << tree.at(f).length << " ";
-			cout << "Feature: " << tree.at(f).feature << " " << endl;
+			cout << "Connection Point: " << newTree.branches.at(f).connection << " ";
+			cout << "X Angle: " << newTree.branches.at(f).xAngle << " ";
+			cout << "Y Angle: " << newTree.branches.at(f).yAngle << " ";
+			cout <<  "Length: " << newTree.branches.at(f).length << " ";
+			cout << "Feature: " << newTree.branches.at(f).feature << " " << endl;
 			cout << "Children: ";
-			for(int g=0;g<tree.at(f).children.size();g++)
+			for(int g=0;g<newTree.branches.at(f).children.size();g++)
 			{
-				cout << tree.at(f).children.at(g) <<" ";
+				cout << newTree.branches.at(f).children.at(g) <<" ";
 			}
 			cout << endl;
 		}
-	cout << "Sunlight: " << sunlight << " Water: " << water << " Phosphorus: " << phosphorus << " Potassium: " << potassium << " Nitrogen: " << nitrogen << endl;
+		cout << "Sunlight: " << newTree.sunlight << " Water: " << newTree.water << " Phosphorus: " << newTree.phosphorus << " Potassium: " << newTree.potassium << " Nitrogen: " << newTree.nitrogen << endl;
 	}
 }
