be424e3592c888329e6edcb04da1010cc2b52182
trewqpoiuuy <bknutson@bayschoolsf.org>
2014-01-23 12:49:55 -0800

    integrated will's resource system

--- Tree generation test.cpp
+++ Tree generation test.cpp
@@ -2,89 +2,23 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <Tree generation.h>
+#include "VectorLib.h"
+#include "Tree generation.h"
+
 using namespace std;
-seed generateSeed()
-{
-	seed treeSeed;
-	/*
-		~See Tree generation.h for the significance of these values~
-	*/
-	treeSeed.branchDensity=randFloat(0.5,2);
-	treeSeed.angleVariance=randFloat(10,30);
-	treeSeed.featureChance=randFloat(0,1);
-	treeSeed.lengthVariance=randFloat(0,2);
-	treeSeed.primaryColor[0]=randInt(0,255);
-	treeSeed.primaryColor[1]=randInt(0,255);
-	treeSeed.primaryColor[2]=randInt(0,255);
-	treeSeed.secondaryColor[0]=randInt(0,255);
-	treeSeed.secondaryColor[1]=randInt(0,255);
-	treeSeed.secondaryColor[2]=randInt(0,255);
-	treeSeed.tertiaryColor[0]=randInt(0,255);
-	treeSeed.tertiaryColor[1]=randInt(0,255);
-	treeSeed.tertiaryColor[2]=randInt(0,255);
-	cout << "Seed Data:" << endl;
-	cout << "Primary: " << treeSeed.primaryColor[0] << " " << treeSeed.primaryColor[1] << " " << treeSeed.primaryColor[2] << endl;
-	cout << "Secondary: " << treeSeed.secondaryColor[0] << " " << treeSeed.secondaryColor[1] << " " << treeSeed.secondaryColor[2] << endl;
-	cout << "Tertiary: " << treeSeed.tertiaryColor[0] << " " << treeSeed.tertiaryColor[1] << " " << treeSeed.tertiaryColor[2] << endl;
-	cout << "Branch Density: " << treeSeed.branchDensity << " Angle Variance: " << treeSeed.angleVariance << " Feature Chance: "<< treeSeed.featureChance << " Length Variance: " << treeSeed.lengthVariance << endl;
-	return treeSeed;
-}
-tree spawnTree(int x, int y,int z,seed treeSeed)
-{	
-	tree newTree;
-	newTree.sunlight=0;   //
-	newTree.water=0;      //
-	newTree.phosphorus=0; //Resources
-	newTree.nitrogen=0;   //
-	newTree.potassium=0;  //
-	newTree.isAlive=1;
-	newTree.x=x;
-	newTree.y=y;
-	newTree.z=z;
-	newTree.treeSeed=treeSeed;
-	branch trunk;
-	trunk.connection=0;
-	trunk.xAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
-	trunk.yAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
-	trunk.length=randFloat(1,40);
-	trunk.feature=0;
-	newTree.branches.push_back(trunk);
-	return newTree;
-}
-tree growBranch(tree newTree)
-{
-	branch newBranch;
-	int branchWeighting=randInt(0,newTree.branches.size())*newTree.treeSeed.branchDensity; //Weights connection points
-	if(branchWeighting>=newTree.branches.size())
-	{
-		branchWeighting=newTree.branches.size()-1; //Makes sure branches only connect to existing branches
-	}
-	newBranch.connection=(newTree.branches.size()-branchWeighting);
-	newTree.branches.at(newBranch.connection-1).children.push_back(newTree.branches.size()+1);
-	newBranch.xAngle=randFloat(-newTree.treeSeed.angleVariance,newTree.treeSeed.angleVariance);
-	newBranch.yAngle=randFloat(-newTree.treeSeed.angleVariance,newTree.treeSeed.angleVariance);
-	newBranch.length=randFloat(1,20)*newTree.treeSeed.lengthVariance;
-	newBranch.feature=0;
-	int featureChance=rand()%100;
-	if(featureChance/100<newTree.treeSeed.featureChance)
-	{
-		newBranch.feature=randInt(1,3);
-	}
-	newTree.branches.push_back(newBranch);
-	newTree.sunlight=newTree.sunlight-newBranch.length/2;
-	newTree.water=newTree.water-newBranch.length;
-	newTree.nitrogen=newTree.nitrogen-newBranch.length/1.5; //Resource usage based solely on length for now. This will likely change.
-	newTree.potassium=newTree.potassium-newBranch.length*1.5;
-	newTree.phosphorus=newTree.phosphorus-newBranch.length*1.25;
-	return newTree;
-}
+using namespace veclib;
+
 int main()
 {
 	srand(time(NULL));
 	int uselessThing=rand(); //First value of rand is the seed, so this gets rid of it
+	int depth = 10;
+	int length = 100;
+	int width = 100;
+	int TopsoilDepth = 3;
+	vector<string> ResourceVector=initializeResources(depth,length,width,TopsoilDepth);
 	seed treeSeed=generateSeed();
-	tree newTree=spawnTree(0,0,0,treeSeed);
+	tree newTree=spawnTree(0,0,0,treeSeed, width, length, depth, ResourceVector);
 	while(newTree.isAlive==1)
 	{
 		int sunlightAdded;
@@ -98,18 +32,22 @@ int main()
 		cout << "water this turn?" << endl;
 		cin >> waterAdded;
 		newTree.water=newTree.water+waterAdded;
+		ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "water", waterAdded);
 		cout << "potassium this turn?" << endl;				//Prompts are temporary, will be replaced by game engine commands
 		cin >> potassiumAdded;
 		newTree.potassium=newTree.potassium+potassiumAdded;
+		ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "potassium", potassiumAdded);
 		cout << "phosphorus this turn?" << endl;
 		cin >> phosphorusAdded;
 		newTree.phosphorus=newTree.phosphorus+phosphorusAdded;
+		ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "phosphorus", phosphorusAdded);
 		cout << "nitrogen this turn?" << endl;
 		cin >> nitrogenAdded;
 		newTree.nitrogen=newTree.nitrogen+nitrogenAdded;
+		ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "nitrogen", nitrogenAdded);
 			while(newTree.sunlight>=10 && newTree.water>=20 && newTree.nitrogen>=15 && newTree.potassium>=30 && newTree.phosphorus>=25)
 			{
-				newTree=growBranch(newTree);
+				newTree=growBranch(newTree, ResourceVector, width, length, depth);
 			}
 		for(int f=0;f<newTree.branches.size();f++) //prints out tree data
 		{
--- Tree generation.h
+++ Tree generation.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+using namespace veclib;
 using namespace std;
 float randFloat(float min, float max)
 {
@@ -48,3 +49,82 @@ struct tree
 	vector<branch> branches;
 	seed treeSeed;
 };
+seed generateSeed()
+{
+	seed treeSeed;
+	/*
+		~See Tree generation.h for the significance of these values~
+	*/
+	treeSeed.branchDensity=randFloat(0.5,2);
+	treeSeed.angleVariance=randFloat(10,30);
+	treeSeed.featureChance=randFloat(0,1);
+	treeSeed.lengthVariance=randFloat(0,2);
+	treeSeed.primaryColor[0]=randInt(0,255);
+	treeSeed.primaryColor[1]=randInt(0,255);
+	treeSeed.primaryColor[2]=randInt(0,255);
+	treeSeed.secondaryColor[0]=randInt(0,255);
+	treeSeed.secondaryColor[1]=randInt(0,255);
+	treeSeed.secondaryColor[2]=randInt(0,255);
+	treeSeed.tertiaryColor[0]=randInt(0,255);
+	treeSeed.tertiaryColor[1]=randInt(0,255);
+	treeSeed.tertiaryColor[2]=randInt(0,255);
+	cout << "Seed Data:" << endl;
+	cout << "Primary: " << treeSeed.primaryColor[0] << " " << treeSeed.primaryColor[1] << " " << treeSeed.primaryColor[2] << endl;
+	cout << "Secondary: " << treeSeed.secondaryColor[0] << " " << treeSeed.secondaryColor[1] << " " << treeSeed.secondaryColor[2] << endl;
+	cout << "Tertiary: " << treeSeed.tertiaryColor[0] << " " << treeSeed.tertiaryColor[1] << " " << treeSeed.tertiaryColor[2] << endl;
+	cout << "Branch Density: " << treeSeed.branchDensity << " Angle Variance: " << treeSeed.angleVariance << " Feature Chance: "<< treeSeed.featureChance << " Length Variance: " << treeSeed.lengthVariance << endl;
+	return treeSeed;
+}
+tree spawnTree(int x, int y, int z, seed treeSeed, int width, int length, int depth, vector<string> ResourceVector)
+{	
+	tree newTree;
+	newTree.sunlight=0;   //
+	newTree.water=WaterGrab(x, y, z, width, length, depth, ResourceVector);      //
+	newTree.phosphorus=PhosphorusGrab(x, y, z, width, length, depth, ResourceVector); //Resources
+	newTree.nitrogen=NitrogenGrab(x, y, z, width, length, depth, ResourceVector);   //
+	newTree.potassium=PotassiumGrab(x, y, z, width, length, depth, ResourceVector);  //
+	newTree.isAlive=1;
+	newTree.x=x;
+	newTree.y=y;
+	newTree.z=z;
+	newTree.treeSeed=treeSeed;
+	branch trunk;
+	trunk.connection=0;
+	trunk.xAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
+	trunk.yAngle=randFloat(-treeSeed.angleVariance,treeSeed.angleVariance);
+	trunk.length=randFloat(1,40);
+	trunk.feature=0;
+	newTree.branches.push_back(trunk);
+	return newTree;
+}
+tree growBranch(tree newTree, vector<string> ResourceVector,int width, int length, int depth)
+{
+	branch newBranch;
+	int branchWeighting=randInt(0,newTree.branches.size())*newTree.treeSeed.branchDensity; //Weights connection points
+	if(branchWeighting>=newTree.branches.size())
+	{
+		branchWeighting=newTree.branches.size()-1; //Makes sure branches only connect to existing branches
+	}
+	newBranch.connection=(newTree.branches.size()-branchWeighting);
+	newTree.branches.at(newBranch.connection-1).children.push_back(newTree.branches.size()+1);
+	newBranch.xAngle=randFloat(-newTree.treeSeed.angleVariance,newTree.treeSeed.angleVariance);
+	newBranch.yAngle=randFloat(-newTree.treeSeed.angleVariance,newTree.treeSeed.angleVariance);
+	newBranch.length=randFloat(1,20)*newTree.treeSeed.lengthVariance;
+	newBranch.feature=0;
+	int featureChance=rand()%100;
+	if(featureChance/100<newTree.treeSeed.featureChance)
+	{
+		newBranch.feature=randInt(1,3);
+	}
+	newTree.branches.push_back(newBranch);
+	newTree.sunlight=newTree.sunlight-newBranch.length/2;
+	newTree.water=newTree.water-newBranch.length;
+	ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "water", newBranch.length);
+	newTree.nitrogen=newTree.nitrogen-newBranch.length/1.5; //Resource usage based solely on length for now. This will likely change.
+	ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "nitrogen", newBranch.length/1.5);
+	newTree.potassium=newTree.potassium-newBranch.length*1.5;
+	ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "potassium", newBranch.length*1.5);
+	newTree.phosphorus=newTree.phosphorus-newBranch.length*1.25;
+	ResourceChange(newTree.x, newTree.y, newTree.z, width, length, depth, ResourceVector, "phosphorus", newBranch.length*1.25);
+	return newTree;
+}
