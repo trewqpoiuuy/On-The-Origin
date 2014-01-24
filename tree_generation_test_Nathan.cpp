76914feba006c89f15420fd1d7f97bb7ee0d82a1
Nathan Siegel <siegelnathan08@gmail.com>
2014-01-23 23:06:13 -0800

    Tree Generation Should work properly now
    Still not ready to be integrated
    Modifications made to both the BRANCH struct and the SEED struct
    Can probably be made to use less memory

--- tree_generation_test_Nathan.cpp
+++ tree_generation_test_Nathan.cpp
@@ -25,6 +25,9 @@ struct branch
 	float length;
 	int feature;
 	vector<int> children;
+	vector<float> xyzPos;
+	vector<float> pRot;
+	
 };
 struct seed
 {
@@ -34,10 +37,12 @@ struct seed
 	int primaryColor[3];
 	int secondaryColor[3];
 	int tertiaryColor[3];
+	float maxvals[3];
 };
 
 vector<branch> tree;
 branch trunk;
+seed treeSeed;
 
 float randFloat(float min, float max)
 {
@@ -164,6 +169,13 @@ float degtorad(float a)
 	return a;
 }
 
+float radtodeg(float a)
+{
+	a = (a / 3.14159265) * 180;
+	return a;
+}
+
+//calculates Cartesian coordinates from Polar coordinates
 vector<float> calcTran (float length, float xAng, float yAng) //Calculates 3-d transforms for each 
 {
 	float xTran, yTran, zTran;
@@ -183,6 +195,83 @@ vector<float> calcTran (float length, float xAng, float yAng) //Calculates 3-d t
 }
 
 float angle = 0.0f;
+float tRot = 0.0f;
+
+
+vector<branch> CalcXYZ(vector<branch> Wtree)
+{
+	for (int d = 0; d < Wtree.size();)
+	{
+		while (Wtree.at(d).xyzPos.size() < 3)
+		{
+			Wtree.at(d).xyzPos.push_back(0);
+		}
+		Wtree.at(d).xyzPos.at(0) = 0;
+		Wtree.at(d).xyzPos.at(1) = 0;
+		Wtree.at(d).xyzPos.at(2) = 0;
+
+		while (Wtree.at(d).pRot.size() < 2)
+		{
+			Wtree.at(d).pRot.push_back(0);
+		}
+		Wtree.at(d).pRot.at(0) = 0;
+		Wtree.at(d).pRot.at(0) = 0;
+
+		d++;
+	}
+	treeSeed.maxvals[0] = 0;
+	treeSeed.maxvals[1] = 0;
+	treeSeed.maxvals[2] = 0;
+	for (int f = 0; f<Wtree.size();)
+	{
+		
+		vector<float> xyzPos(3);
+		xyzPos.at(0) = 0;
+		xyzPos.at(1) = 0;
+		xyzPos.at(2) = 0;
+
+		float pyRot = Wtree.at(f).yAngle;
+		float pxRot = Wtree.at(f).xAngle;
+		pxRot += Wtree.at(f).pRot.at(0);
+		pyRot += Wtree.at(f).pRot.at(1);
+		//cout << Wtree.at(f).pRot.at(1) << endl;
+		float length = Wtree.at(f).length;
+		vector<int> children = Wtree.at(f).children;
+		
+		xyzPos = calcTran(length, pxRot, pyRot);
+
+		Wtree.at(f).xyzPos.at(0) += xyzPos.at(0);
+		Wtree.at(f).xyzPos.at(1) += xyzPos.at(1);
+		Wtree.at(f).xyzPos.at(2) += xyzPos.at(2);
+		
+
+		for (int i = 0; i < children.size();)
+		{
+			int child = Wtree.at(f).children.at(i);
+			
+			Wtree.at(child).pRot.at(0) += pxRot;
+			Wtree.at(child).pRot.at(1) += pyRot;
+			Wtree.at(child).xyzPos.at(0) += Wtree.at(f).xyzPos.at(0);
+			Wtree.at(child).xyzPos.at(1) += Wtree.at(f).xyzPos.at(1);
+			Wtree.at(child).xyzPos.at(2) += Wtree.at(f).xyzPos.at(2);
+			i++;
+		}
+		if (treeSeed.maxvals[0] < Wtree.at(f).xyzPos.at(0))
+		{
+			treeSeed.maxvals[0] = Wtree.at(f).xyzPos.at(0);
+		}
+		if (treeSeed.maxvals[1] < Wtree.at(f).xyzPos.at(1))
+		{
+			treeSeed.maxvals[1] = Wtree.at(f).xyzPos.at(1);
+		}
+		if (treeSeed.maxvals[2] < Wtree.at(f).xyzPos.at(2))
+		{
+			treeSeed.maxvals[2] = Wtree.at(f).xyzPos.at(2);
+		}
+		f++;
+	}
+	return Wtree;
+}
 
 void renderScene(void) {
 
@@ -211,67 +300,46 @@ void renderScene(void) {
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glutSolidCube(0.3f);
 
-	for (int f = 0; f<tree.size();)
+	
+	
+	for (int f = 0; f < tree.size();)
 	{
-		vector<float> xyzPos(3);
-		xyzPos.at(0) = 0;
-		xyzPos.at(1) = 0;
-		xyzPos.at(2) = 0;
-
-		float yRot = tree.at(f).yAngle;
-		float xRot = tree.at(f).xAngle;
-
-		float brLen = 0;
-		
-		float length = tree.at(f).length;
-
-		xyzPos.at(0) += calcTran(length, xRot, yRot).at(0);
-		xyzPos.at(1) += calcTran(length, xRot, yRot).at(1);
-		xyzPos.at(2) += calcTran(length, xRot, yRot).at(2);
-
-		float treeconnection = 0;
-
-		treeconnection = tree.at(f).connection;
-
-		for (int c = f; c > 0; c--) //negating previous translations
+		float colorwheel = (tree.at(f).xyzPos.at(1) / treeSeed.maxvals[1]);
+		//cout << colorwheel << endl;
+		float primColor = 2 * (-colorwheel + 0.5f);
+		float tertColor = 2 * (colorwheel - 0.5f);
+		if (primColor < 0)
 		{
-			if (treeconnection == c)
-			{
-				brLen = tree.at(c-1).length;
-				treeconnection = tree.at(c-1).connection;
-				yRot = tree.at(c - 1).yAngle;
-				xRot = tree.at(c - 1).xAngle;
-				xyzPos.at(0) += calcTran(brLen, xRot, yRot).at(0);
-				xyzPos.at(1) += calcTran(brLen, xRot, yRot).at(1);
-				xyzPos.at(2) += calcTran(brLen, xRot, yRot).at(2);
-			}
+			primColor = 0;
 		}
+		if (tertColor < 0)
+		{
+			tertColor = 0;
+		}
+		float secoColor = 1 - (primColor + tertColor);		
 		
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glTranslatef(xyzPos.at(0)/20, (xyzPos.at(1) /*+ brLen*/)/20, xyzPos.at(2)/20);
+		//cout << treeSeed.primaryColor[0] * primColor << endl;
+		glColor3ub((treeSeed.primaryColor[0] * primColor) + (treeSeed.secondaryColor[0] * secoColor) + (treeSeed.tertiaryColor[0] * tertColor),
+				   (treeSeed.primaryColor[1] * primColor) + (treeSeed.secondaryColor[1] * secoColor) + (treeSeed.tertiaryColor[1] * tertColor),
+				   (treeSeed.primaryColor[2] * primColor) + (treeSeed.secondaryColor[2] * secoColor) + (treeSeed.tertiaryColor[2] * tertColor));
+		
+
+		glTranslatef(tree.at(f).xyzPos.at(0) / 20, tree.at(f).xyzPos.at(1) / 20, tree.at(f).xyzPos.at(2) / 20);
+		//cout << "x pos: " << tree.at(f).xyzPos.at(0) << " y pos: " << tree.at(f).xyzPos.at(1) << "z pos: " << tree.at(f).xyzPos.at(2) << endl;
 		glutSolidSphere(0.25f, 20, 20);
-		/*glRotatef(xRot, 0.0f, 1.0f, 0.0f);
-		glColor3f(0.9f, 0.9f, 0.9f);
-		glutSolidCone(0.25f, length/20, 10, 10);
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glRotatef(-xRot, 0.0f, 1.0f, 0.0f);*/
-		glTranslatef(-xyzPos.at(0)/20, -(xyzPos.at(1) /*+ brLen*/)/20, -xyzPos.at(2)/20);
-		/*cout << "Branch #" << f + 1 << ": ";
-		cout << "Connection Point: " << tree.at(f).connection << " ";
-		cout << "X Angle: " << tree.at(f).xAngle << " ";
-		cout << "Y Angle: " << tree.at(f).yAngle << " ";
-		cout << "Length: " << tree.at(f).length << " ";
-		cout << "Feature: " << tree.at(f).feature << " " << endl;*/
+		glTranslatef(-tree.at(f).xyzPos.at(0) / 20, -tree.at(f).xyzPos.at(1) / 20, -tree.at(f).xyzPos.at(2) / 20);
 		f++;
+
 	}
 
 	glutSwapBuffers();
 }
 
+
 int main(int argc, char **argv)
 {
 	srand(static_cast <unsigned> (time(0)));
-	seed treeSeed;
+	
 	treeSeed.branchDensity = randFloat(0, 2);
 	treeSeed.angleVariance = randFloat(0, 30);
 	treeSeed.featureChance = randFloat(0, 1);
@@ -336,6 +404,12 @@ int main(int argc, char **argv)
 			cout << "Y Angle: " << tree.at(f).yAngle << " ";
 			cout << "Length: " << tree.at(f).length << " ";
 			cout << "Feature: " << tree.at(f).feature << " " << endl;
+			cout << "Children: ";
+			for (int i = 0; i < tree.at(f).children.size(); i++)
+			{
+				cout << tree.at(f).children.at(i) << " : ";
+			}
+			cout << endl;
 		}
 		cout << "Kill? (1 for yes, 0 for no)" << endl;
 		bool isKilled;
@@ -345,6 +419,12 @@ int main(int argc, char **argv)
 			isAlive = 0;
 		}
 	}
+	//calculate all trees
+
+	tree = CalcXYZ(tree);
+
+	//////////////
+
 	cout << "Generation Ended" << endl;
 
 	glutInit(&argc, argv);
