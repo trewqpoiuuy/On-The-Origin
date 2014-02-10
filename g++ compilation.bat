ad44119b467d938d9d6b5d620d3ca02b1fb42c8a
trewqpoiuuy <bknutson@bayschoolsf.org>
2014-02-10 08:40:26 -0800

    commented the .bat file

--- g++ compilation.bat
+++ g++ compilation.bat
@@ -1,3 +1,3 @@
-cd C:\Users\BKnutson\Dropbox\Computer Science\CS 2
+cd C:\Users\BKnutson\Dropbox\Computer Science\CS 2 REM Change this to whatever directory you want the executable to go
 g++ -o TreeTest -I C:\Users\BKnutson\Documents\GitHub\On-The-Origin\Resources -I C:\Users\BKnutson\Documents\GitHub\On-The-Origin\TreeGen "C:\Users\BKnutson\Documents\GitHub\On-The-Origin\Tests\Tree generation test.cpp"
 pause
