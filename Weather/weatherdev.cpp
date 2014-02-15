8b8f41cac91366b18977588871736403215ebfc0
WBoettcher <thantomaniac@gmail.com>
2014-02-14 17:24:29 -0800

    Progress on weather
    moved everything to a target based system
    testing now gives reasonable (not broken) outputs
    added a cycle based on/off system, to supplement halflife; could be used
    to set a cap on the number of cycles a weather phenomenon can remain on
    or off, or to determine how many cycles a weather phenomenon (like sun)
    can go w/out being being turned on and off.

--- Weather/weatherdev.cpp
+++ Weather/weatherdev.cpp
@@ -17,101 +17,161 @@ int main()
 	class Weather {
 	public:
 		bool exists = false;
-		unsigned int intensity = 0;
-		long int intensity_roc = 0;
-		unsigned int halflife;
-		int direction = 180;
-		int direction_roc = 0;
-		unsigned int intensity_max;
-		unsigned int intensitychange_max;
+		unsigned int halflife = 0;
+		unsigned int cyclecounter = 0;
+		unsigned int cyclereset = 0;
 
-		void existanceroll()
-		{
-			unsigned int roll = rand()%halflife;
-			if (roll==(halflife-1))
-			{
-				if (exists == true)
-				{
-					exists = false;
-					intensity = 0;
-				}
-				else
-					exists = true;
-			}
-		}
+		long int intensity = 0;
+		long int intensity_min = 0;
+		long int intensity_max = 10000;
+		long int intensity_target = 0;
+		long int intensitychange_max;
 
-		int directionroll()
-		{
-			int directionchange = rand()%1;
-			int sign = rand()%2;
-			if (sign==1)
-				directionchange=(-1)*directionchange;
-			return directionchange;
-		}
-
-		long int intensityroll()
-		{
-			int intensitychange = rand()%(intensitychange_max/15);
-			int sign = rand()%2;
-			if (sign==1)
-				intensitychange=(-1)*intensitychange;
-			return intensitychange;
-		}
+		float direction = 180;
+		float direction_target = 180;
+		float directionchange_max = 0;
 
 		void cycle()
 		{
-			existanceroll();
-			if (exists == true)
+			//turns the weather pattern on and off at random
+			if (halflife>0)
 			{
-				int directionchange = directionroll();
-				if (direction_roc + directionchange > 5)
-					direction_roc = 5;
-				else if (direction_roc + directionchange < -5)
-					direction_roc = -5;
-				else
-					direction_roc += directionchange;
-
-				long int intensitychange = intensityroll();
-				if (intensity_roc + intensitychange > intensitychange_max)
-					direction_roc = intensitychange_max;
-				else if (intensity_roc + intensitychange < (-1)*intensitychange_max)
-					direction_roc = (-1)*intensitychange_max;
-				else
-					intensity_roc += intensitychange;
-
-				if (direction + direction_roc > 360)
-					direction = (direction + direction_roc)%360;
-				else if (direction + direction_roc < 0)
-					direction = 360+((direction + direction_roc)%360);
-				else
-					direction+=directionchange;
-
-				if (intensity + intensity_roc > intensity_max)
-					intensity = intensity_max;
-				else if (intensity + intensity_roc <= 0)
+				unsigned int existanceroll = rand()%halflife;
+				if (existanceroll == halflife-1)
 				{
-					exists = false;
-					intensity = 0;
+					if (exists == true)
+					{
+						exists = false;
+						//cout << "\ne false";
+					}
+					else
+					{
+						exists = true;
+						//cout << "\ne true";
+					}
+				}
+			}
+			//turns the weather pattern on and off cyclically
+			if (cyclereset > 0)
+			{
+				if (cyclecounter == cyclereset-1)
+				{
+					cyclecounter = 0;
+					if (exists == true)
+					{
+						exists = false;
+						intensity = 0;
+						intensity_target = 0;
+					}
+					else
+					{
+						exists = true;
+						intensity = intensity_min;
+						intensity_target = intensity_min;
+					}
 				}
 				else
-					intensity+=intensity_roc;
+					cyclecounter+=1;
+			}
 
+			//sets target for intensity
+			if (exists == false)
+				intensity_target = 0;
+			else if (intensity == intensity_target)
+				intensity_target = intensity_min + rand()%(intensity_max-intensity_min);
+
+			//moves intensity in the direction of the target
+			if (intensity<intensity_target)
+			{
+				intensity += ((rand()%intensitychange_max)+1);
+				if (intensity>intensity_target) //prevents intensity from overshooting the target
+					intensity = intensity_target;
+			}
+			else if (intensity>intensity_target)
+			{
+				if (exists == false)
+					intensity = intensity/2; //drops intensity exponentially if the weather phenomenon doesn't exist
+				intensity -= ((rand()%intensitychange_max)+1);
+				if (intensity<intensity_target) //prevents intensity from overshooting the target
+					intensity = intensity_target;
+			}
+
+			//checks for the applicability of direction
+			if (directionchange_max > 0)
+			{
+				//sets target for direction
+				if (direction == direction_target)
+					direction_target = (static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/360)));
+
+				//moves direction towards the target
+				if (direction<direction_target)
+				{
+					direction += (static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/directionchange_max))); //random float between 0 and directionchange_max
+						if (direction>direction_target) //prevents direction from overshooting the target
+							direction = direction_target;
+				}
+				else if (direction>direction_target)
+				{
+					direction -= (static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/directionchange_max)));
+					if (direction<direction_target) //prevents direction from overshooting the target
+						direction = direction_target;
+				}
 			}
 		}
-	} test;
+	};
 
-	test.halflife = 20;
-	test.intensity_max = 10000;
-	test.intensitychange_max = 1000;
+	cout << "Start\n";
 
-	for(unsigned int i = 0; i<10000; i++)
+	Weather rain;
+	rain.halflife = 30;
+	rain.intensity_max = 10000;
+	rain.intensitychange_max = 100;
+
+	Weather wind;
+	wind.halflife = 100;
+	wind.intensity_max = 1000;
+	wind.intensitychange_max = 10;
+	wind.directionchange_max = 1;
+
+	Weather sun;
+	sun.cyclereset = 24;
+	sun.intensity_max = 1100;
+	sun.intensity_min = 900;
+	sun.intensitychange_max = 10;
+
+	for(unsigned int i = 0; i<600; i++)
 	{
-		test.cycle();
-		cout << "\nexists: " << test.exists;
-		cout << "\ndirection: " << test.direction;
-		cout << "\nintensity: " << test.intensity;
+		rain.cycle();
 
+		cout << "\nrain e|i|i_t: "
+		<< rain.exists
+		<< "|" << rain.intensity
+		<< "|" << rain.intensity_target;
 	}
+
+	for(unsigned int i = 0; i<600; i++)
+	{
+		wind.cycle();
+
+		cout << "\nwind e|d|d_t|i|i_t: "
+		<< wind.exists
+		<< "|" << wind.direction
+		<< "|" << wind.direction_target
+		<< "|" << wind.intensity
+		<< "|" << wind.intensity_target;
+	}
+
+	for(unsigned int i = 0; i<600; i++)
+	{
+		sun.cycle();
+
+		cout << "\nsun e|c|i|i_t: "
+		<< sun.exists
+		<< "|" << sun.cyclecounter
+		<< "|" << sun.intensity
+		<< "|" << sun.intensity_target;
+	}
+	cout <<"\nRAND_MAX: " << RAND_MAX;
 };
 
 
